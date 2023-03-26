48d5b88a96ff424fe5360b78c02c043050861aae
farhan0184 <shahariar0184@gmail.com>
2023-03-26 19:56:54 +0600

    add: p tag in Not found page

--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -5,6 +5,7 @@ const NotFound = () => {
         <div className='text-center text-3xl text-gray-600 mt-5 italic'>
             <h1>Page is not found</h1>
             <h1>400</h1>
+            <p>hello world</p>
         </div>
     );
 };
